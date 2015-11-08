60ef04875623f455a40ee95acb54bb2eddfef114
LaurentGrima <grima.laurent@gmail.com>
2015-11-08 23:30:20 +0100

    some more methods

--- models/thread.py
+++ models/thread.py
@@ -5,3 +5,17 @@ class Thread(object):
 
     def participants(self):
         return self.soup.contents[0].replace('  ', '').replace('\n', '')
+
+    def messages_meta(self, text_only=False):
+        metas = self.soup.find_all('div',
+                                   {'class': 'message'},
+                                   recursive=False)
+        return metas
+
+    def messages_text(self, text_only=False):
+        texts = self.soup.find_all('p', recursive=False)
+        return texts
+
+    @staticmethod
+    def extract_text(soup_list):
+        return [elem.text for elem in soup_list]
