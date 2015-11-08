771cf013ed92da9d9a91444f70db49d88b52e74f
LaurentGrima <grima.laurent@gmail.com>
2015-11-08 20:42:27 +0100

    Tooling around with beautifulsoup

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
+from models.thread import Thread
 
 
-# Do some stuff
-file_path = 'data/messages.htm'
+file_path = 'data/sample.html'
 f = open(file_path, 'r')
-html_content = f.read(1000)
-soup = BeautifulSoup(html_content)
-print(soup)
+raw_html_content = f.read()
+soup = BeautifulSoup(raw_html_content, 'html.parser')
+body = soup.html.body
+contents = body.find('div', {'class': 'contents'}, recursive=False)
+user = contents.h1.text
+threads = contents.div.find_all('div', {'class': 'thread'}, recursive=False)
+
+for thread_soup in threads:
+    thread = Thread(thread_soup)
+    print(thread.participants())
