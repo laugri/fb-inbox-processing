a88f595a23934f9efebc156d5f240efba2a1e99e
LaurentGrima <grima.laurent@gmail.com>
2015-12-31 18:57:23 +0100

    Some unit tests.

---
+++ fbinboxprocessing/tests/test_thread.py
@@ -0,0 +1,10 @@
+import unittest
+
+from fbinboxprocessing.models.inbox import Inbox
+from fbinboxprocessing.tests import INBOX_SOUP
+
+
+class TestThreadMethods(unittest.TestCase):
+
+    def test_thread_thingy(self):
+        pass
